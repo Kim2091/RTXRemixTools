c4a3960d09b373f7b7b6bc3cee52334fb05ecb05
Kim2091 <62084776+Kim2091@users.noreply.github.com>
2023-06-29 20:49:26 -0400

    Update MagicUSDA.py

--- MagicUSDA/MagicUSDA.py
+++ MagicUSDA/MagicUSDA.py
@@ -1,55 +1,52 @@
-import argparse
-import os
+import os, argparse
 import xxhash
+from pxr import Usd, UsdGeom, UsdShade, Sdf
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--directory', help='Path to directory', required=True)
-parser.add_argument('-o', '--output', help='Output file name', default='mod')
-parser.add_argument('-g', '--generate-hashes', help='Generate hashes for file names', action='store_true')
-parser.add_argument('-m', '--multiple-files', help='Save multiple .usda files, one for each suffix type (except for diffuse)', action='store_true')
-parser.add_argument('-a', '--add-sublayers', action='store_true', help='Add sublayers made with -m to the mod.usda file. This argument only modifies the mod.usda file and does not affect any custom USDA file specified by the -o argument.')
-args = parser.parse_args()
+suffixes = ["_normal", "_emissive", "_metallic", "_rough"]
 
-directory_path = args.directory
-game_ready_assets_path = os.path.join(directory_path)
-
-suffixes = ['_normal', '_emissive', '_metallic', '_rough']
-
-def generate_hashes(file_path):
+def generate_hashes(file_path) -> str:
     # Read the file and extract the raw data. Thanks @BlueAmulet!
-    with open(file_path, 'rb') as file:
-        data = file.read()
-        
+    with open(file_path, "rb") as file:
+        data = file.read(128)
+
     dwHeight = int.from_bytes(data[12:16], "little")
-    dwWidth  = int.from_bytes(data[16:20], "little")
-    pfFlags  = int.from_bytes(data[80:84], "little")
+    dwWidth = int.from_bytes(data[16:20], "little")
+    pfFlags = int.from_bytes(data[80:84], "little")
     pfFourCC = data[84:88]
     bitCount = int.from_bytes(data[88:92], "little")
-        
-    mipsize = dwWidth*dwHeight
-    if pfFlags & 0x4: # DDPF_FOURCC
-        if pfFourCC == b'DXT1': # DXT1 is 4bpp
+
+    mipsize = dwWidth * dwHeight
+    if pfFlags & 0x4:  # DDPF_FOURCC
+        if pfFourCC == b"DXT1":  # DXT1 is 4bpp
             mipsize //= 2
-    elif pfFlags & 0x20242: # DDPF_ALPHA | DDPF_RGB | DDPF_YUV | DDPF_LUMINANCE
-        mipsize = mipsize*bitCount//8
-        
-    hash_value = xxhash.xxh3_64(data[128:128+mipsize]).hexdigest()
+    elif pfFlags & 0x20242:  # DDPF_ALPHA | DDPF_RGB | DDPF_YUV | DDPF_LUMINANCE
+        mipsize = mipsize * bitCount // 8
+
+    # Read the required portion of the file for hash calculation
+    with open(file_path, "rb") as file:
+        file.seek(128)  # Move the file pointer to the appropriate position
+        data = file.read(mipsize)
+
+    hash_value = xxhash.xxh3_64(data).hexdigest()
 
     return hash_value.upper()
 
-created_files = []
-modified_files = []
 
-def write_usda_file(suffix=None):
+def write_usda_file(args, file_list, suffix=None) -> [list, list]:
+    created_files  = []
+    modified_files = []
+    game_ready_assets_path = os.path.join(args.directory)
+
     # Check if there are any texture files with the specified suffix
     if suffix:
         has_suffix_files = False
-        for file_name in os.listdir(game_ready_assets_path):
-            if file_name.endswith(f'{suffix}.dds'):
+        for file_name in file_list:
+            if file_name.endswith(f"{suffix}.dds"):
                 has_suffix_files = True
                 break
         if not has_suffix_files:
-            return
+            # return a blank set
+            return [created_files, modified_files]
 
     usda_file_name = f'{args.output}{suffix if suffix else ""}.usda'
     usda_file_path = os.path.join(game_ready_assets_path, usda_file_name)
@@ -59,205 +56,190 @@ def write_usda_file(suffix=None):
     else:
         created_files.append(usda_file_path)
 
-    hashes = set()
-    
-    for file_name in os.listdir(game_ready_assets_path):
-        if file_name.endswith('.dds'):
+    targets = {}
+    for file_name in file_list:
+        if file_name.endswith(".dds"):
             name, ext = os.path.splitext(file_name)
-            if '_' not in name:
-                hashes.add(name)
-            else:
-                name, file_suffix = name.rsplit('_', 1)
-                if (suffix and file_suffix == suffix) or (not suffix and (file_suffix in suffixes or file_suffix == 'diffuse')):
-                    hashes.add(name)
-
-    with open(usda_file_path, 'w') as usda_file:
-        usda_file.write(f'''#usda 1.0
-(
-    upAxis = "Y"
-)
-
-def Scope "Looks"
-{{
-}}
-
-over "RootNode"
-{{
-    over "Looks"
-    {{''')
-        
-        added_hashes = set()
-        for hash in hashes:
-            # Check if there is a corresponding texture file for the specified suffix
-            if suffix and not any(file_name.endswith(f'{hash}{suffix}.dds') for file_name in os.listdir(game_ready_assets_path)):
-                continue
-
-            if args.generate_hashes:
-                if f'{hash}.dds' in os.listdir(game_ready_assets_path):
-                    hash_value = generate_hashes(os.path.join(game_ready_assets_path, f'{hash}.dds'))
-                elif f'{hash}_diffuse.dds' in os.listdir(game_ready_assets_path):
-                    hash_value = generate_hashes(os.path.join(game_ready_assets_path, f'{hash}_diffuse.dds'))
+            if "_" not in name or name.endswith("_diffuse"):
+                # Check if the skip_hash_generation argument is specified
+                if not args.skip_hash_generation:
+                    key = generate_hashes(os.path.join(game_ready_assets_path, file_name))
                 else:
-                    hash_value = hash
-            else:
-                hash_value = hash
+                    key = name
+                targets[key] = name
 
-            if hash_value in added_hashes:
+    # Create a new stage
+    stage = Usd.Stage.CreateNew(usda_file_path)
+
+    # Modify the existing RootNode prim
+    root_node_prim = stage.OverridePrim("/RootNode")
+
+    # Add a Looks scope as a child of the RootNode prim
+    looks_scope = UsdGeom.Scope.Define(stage, "/RootNode/Looks")
+    
+    added_targets = set()
+    for value, name in targets.items():
+        # We only want to generate materials for inputs that are hashes in this mode
+        if args.exclude_non_hashes:
+            if not name.isupper() and not len(name) == 16:
                 continue
-            added_hashes.add(hash_value)
 
-            usda_file.write(f'''
-        over "mat_{hash_value.upper()}"
-        {{
-            over "Shader"
-            {{''')
+        # Check if there is a corresponding texture file for the specified suffix
+        if suffix and not any(
+            file_name.endswith(f"{value}{suffix}.dds") for file_name in file_list
+        ):  continue
+        if value in added_targets:
+            continue
+        else:
+            added_targets.add(value)
+            print(f"Adding: {value}, {name}")
 
+        # Add a material prim as a child of the Looks scope
+        material_prim = UsdShade.Material.Define(
+            stage, f"/RootNode/Looks/mat_{value.upper()}"
+        )
+        material_prim.GetPrim().GetReferences().SetReferences([])
 
-            if not suffix or suffix == '_diffuse':
-                if f'{hash}.dds' in os.listdir(game_ready_assets_path):
-                    usda_file.write(f'''
-                asset inputs:diffuse_texture = @./{hash}.dds@ (
-                    customData = {{
-                        asset default = @@
-                    }}
-                    displayGroup = "Diffuse"
-                    displayName = "Albedo Map"
-                    doc = "The texture specifying the albedo value and the optional opacity value to use in the alpha channel"
-                    hidden = false
-                )''')
+        # Set the shader attributes
+        shader_prim = UsdShade.Shader.Define(
+            stage, f"/RootNode/Looks/mat_{value.upper()}/Shader"
+        )
+        shader_prim.CreateInput("info:mdl:sourceAsset", Sdf.ValueTypeNames.Asset).Set(
+            "AperturePBR_Opacity.mdl"
+        )
+        shader_output = shader_prim.CreateOutput("output", Sdf.ValueTypeNames.Token)
 
-                elif f'{hash}_diffuse.dds' in os.listdir(game_ready_assets_path):
-                    usda_file.write(f'''
-                asset inputs:diffuse_texture = @./{hash}_diffuse.dds@ (
-                    customData = {{
-                        asset default = @@
-                    }}
-                    displayGroup = "Diffuse"
-                    displayName = "Albedo Map"
-                    doc = "The texture specifying the albedo value and the optional opacity value to use in the alpha channel"
-                    hidden = false
-                )''')
-
-            if not suffix or suffix == '_emissive':
-                if f'{hash}_emissive.dds' in os.listdir(game_ready_assets_path):
-                    usda_file.write(f'''
-                asset inputs:emissive_mask_texture = @./{hash}_emissive.dds@ (
-                    colorSpace = "auto"
-                    customData = {{
-                        asset default = @@
-                    }}
-                    displayGroup = "Emissive"
-                    displayName = "Emissive Mask Map"
-                    doc = "The texture masking the emissive color"
-                    hidden = false
-                )
-                bool inputs:enable_emission = 1 (
-                    customData = {{
-                        bool default = 0
-                    }}
-                    displayGroup = "Emissive"
-                    displayName = "Enable Emission"
-                    doc = "Enables the emission of light from the material"
-                    hidden = false
-                )
-                float inputs:emissive_intensity = 5 (
-                    customData = {{
-                        float default = 40
-                        dictionary range = {{
-                            float max = 65504
-                            float min = 0
-                        }}
-                    }}
-                    displayGroup = "Emissive"
-                    displayName = "Emissive Intensity"
-                    doc = "Intensity of the emission"
-                    hidden = false
-                )''')
-
-            if not suffix or suffix == '_metallic':
-                if f'{hash}_metallic.dds' in os.listdir(game_ready_assets_path):
-                    usda_file.write(f'''
-                asset inputs:metallic_texture = @./{hash}_metallic.dds@ (
-                    colorSpace = "auto"
-                    customData = {{
-                        asset default = @@
-                    }}
-                    displayGroup = "Specular"
-                    displayName = "Metallic Map"
-                    hidden = false
-                )''')
-
-            if not suffix or suffix == '_normal':
-                if f'{hash}_normal.dds' in os.listdir(game_ready_assets_path):
-                    usda_file.write(f'''
-                asset inputs:normalmap_texture = @./{hash}_normal.dds@''')
-
-            if not suffix or suffix == '_rough':
-                if f'{hash}_rough.dds' in os.listdir(game_ready_assets_path):
-                    usda_file.write(f'''
-                asset inputs:reflectionroughness_texture = @./{hash}_rough.dds@ (
-                    colorSpace = "auto"
-                    customData = {{
-                        asset default = @@
-                    }}
-                    displayGroup = "Specular"
-                    displayName = "Roughness Map"
-                    hidden = false
-                )''')
-            
-            usda_file.write('''
-            }
-        }''')
+        if not suffix or suffix == "_diffuse":
+            diffuse_texture = shader_prim.CreateInput(
+                "diffuse_texture", Sdf.ValueTypeNames.Asset
+            )
+            diffuse_texture.Set(f"./{name}.dds")
         
-        usda_file.write('''
-    }
-}
-''')
+        # Process each type of texture
+        if not suffix or suffix == "_emissive":
+            emissive_file_name = f"{value}_emissive.dds"
+            if emissive_file_name in file_list:
+                emissive_mask_texture = shader_prim.CreateInput(
+                    "emissive_mask_texture", Sdf.ValueTypeNames.Asset
+                )
+                emissive_mask_texture.Set(f"./{emissive_file_name}")
+                enable_emission = shader_prim.CreateInput(
+                    "enable_emission", Sdf.ValueTypeNames.Bool
+                )
+                enable_emission.Set(True)
+                emissive_intensity = shader_prim.CreateInput(
+                    "emissive_intensity", Sdf.ValueTypeNames.Float
+                )
+                emissive_intensity.Set(5)
 
+        if not suffix or suffix == "_metallic":
+            if f"{name}_metallic.dds" in file_list:
+                metallic_texture = shader_prim.CreateInput(
+                    "metallic_texture", Sdf.ValueTypeNames.Asset
+                )
+                metallic_texture.Set(f"./{name}_metallic.dds")
 
-def add_sublayers():
-    mod_file_name = 'mod.usda'
-    mod_file_path = os.path.join(game_ready_assets_path, mod_file_name)
+        if not suffix or suffix == "_normal":
+            if f"{name}_normal.dds" in file_list:
+                normalmap_texture = shader_prim.CreateInput(
+                    "normal_texture", Sdf.ValueTypeNames.Asset
+                )
+                normalmap_texture.Set(f"./{name}_normal.dds")
+
+        if not suffix or suffix == "_rough":
+            if f"{name}_rough.dds" in file_list:
+                reflectionroughness_texture = shader_prim.CreateInput(
+                    "roughness_texture", Sdf.ValueTypeNames.Asset
+                )
+                reflectionroughness_texture.Set(f"./{name}_rough.dds")
+
+        # Connect shader output to material inputs
+        material_prim.CreateInput(
+            "mdl:displacement", Sdf.ValueTypeNames.Token
+        ).ConnectToSource(shader_output)
+        material_prim.CreateInput(
+            "mdl:surface", Sdf.ValueTypeNames.Token
+        ).ConnectToSource(shader_output)
+        material_prim.CreateInput(
+            "mdl:volume", Sdf.ValueTypeNames.Token
+        ).ConnectToSource(shader_output)
+
+    # Save the stage
+    stage.Save()
+    
+    return [modified_files, created_files]
+
+def add_sublayers(args, file_list) -> list:
+    modified_files = []
+    game_ready_assets_path = os.path.join(args.directory)
+    mod_file_path = os.path.join(game_ready_assets_path, "mod.usda")
     if os.path.exists(mod_file_path):
         modified_files.append(mod_file_path)
-        with open(mod_file_path, 'r') as mod_file:
-            mod_file_content = mod_file.read()
-            sublayer_start = mod_file_content.find('subLayers = [')
-            if sublayer_start != -1:
-                sublayer_end = mod_file_content.find(']', sublayer_start)
-                existing_sublayers = mod_file_content[sublayer_start+len('subLayers = ['):sublayer_end].split('\n')
-                existing_sublayers = [sublayer.strip().rstrip(',') for sublayer in existing_sublayers if sublayer.strip()]
-                new_sublayers = [f'@./{args.output}{suffix}.usda@' for suffix in suffixes if f'@./{args.output}{suffix}.usda@' not in existing_sublayers and os.path.exists(os.path.join(game_ready_assets_path, f'{args.output}{suffix}.usda'))]
-                if new_sublayers:
-                    sublayers = ',\n        '.join(existing_sublayers + new_sublayers)
-                    mod_file_content = mod_file_content[:sublayer_start+len('subLayers = [')] + f'\n        {sublayers}\n    ' + mod_file_content[sublayer_end:]
-                    with open(mod_file_path, 'w') as mod_file:
-                        mod_file.write(mod_file_content)
-            else:
-                sublayers = ',\n        '.join([f'@./{args.output}{suffix}.usda@' for suffix in suffixes if os.path.exists(os.path.join(game_ready_assets_path, f'{args.output}{suffix}.usda'))])
-                with open(mod_file_path, 'w') as mod_file:
-                    mod_file.write(f'''#usda 1.0
-(
-    subLayers = [
-        {sublayers}
-    ]
-)
 
-{mod_file_content}''')
+        # Open the existing stage
+        stage = Usd.Stage.Open(mod_file_path)
 
-if args.add_sublayers:
-    add_sublayers()
+        # Get the existing sublayers
+        existing_sublayers = list(stage.GetRootLayer().subLayerPaths)
 
-if args.multiple_files:
-    for suffix in suffixes:
-        write_usda_file(suffix)
-else:
-    write_usda_file()
+        # Create a set of existing sublayer file names
+        existing_sublayer_files = {
+            os.path.basename(sublayer_path) for sublayer_path in existing_sublayers
+        }
 
-print("Finished!")
-print("Created files:")
-for file in created_files:
-    print(f"  - {file}")
-print("Modified files:")
-for file in modified_files:
-    print(f"  - {file}")
+        # Add new sublayers
+        new_sublayers = [
+            f"./{args.output}{suffix}.usda"
+            for suffix in suffixes
+                if  f"{args.output}{suffix}.usda" not in existing_sublayer_files
+                and f"{args.output}{suffix}.usda" in     file_list
+        ]
+        stage.GetRootLayer().subLayerPaths = (existing_sublayers + new_sublayers)
+
+        # Save the stage
+        stage.Save()
+
+    return modified_files
+
+
+if __name__ == "__main__":
+    ## ARGUMENT BLOCK
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", required=True, help="Path to directory")
+    parser.add_argument("-o", "--output", default="mod", help="Output file name")
+    parser.add_argument("-e", "--exclude-non-hashes", action="store_true", help="Exclude any files without a hash from being included in the output .usda file")
+    parser.add_argument("-s", "--skip-hash-generation", action="store_true", help="Skip generating hashes for texture files")
+    parser.add_argument("-m", "--multiple-files", action="store_true", help="Save multiple .usda files, one for each suffix type (except for diffuse)")
+    parser.add_argument("-a", "--add-sublayers", action="store_true", help="Add sublayers made with -m to the mod.usda file. This argument only modifies the mod.usda file and does not affect any custom USDA file specified by the -o argument.")
+    args = parser.parse_args()
+     
+    # Check target processing directory before use
+    if not os.path.isdir(args.directory):
+        raise FileNotFoundError("Specified processing directory (-d) is invalid")
+    
+    file_list = os.listdir(os.path.join(args.directory))
+    created_files  = []
+    modified_files = []
+    
+    # Process sublayer additions
+    if args.add_sublayers:
+        modified_files.extend(add_sublayers(args, file_list))
+    
+    # Generate unique USDA files per suffix type (except diffuse)
+    if args.multiple_files:
+        for suffix in suffixes:
+            m, c = write_usda_file(args, file_list, suffix)
+            modified_files.extend(m), created_files.extend(c)
+    else:  # Generate a single USDA file for all suffixes
+        m, c = write_usda_file(args, file_list)
+        modified_files.extend(m), created_files.extend(c)
+    
+    # Complete
+    print("Finished!")
+    print("Created files:")
+    for file in created_files:
+        print(f"  - {file}")
+    print("Modified files:")
+    for file in modified_files:
+        print(f"  - {file}")
+
